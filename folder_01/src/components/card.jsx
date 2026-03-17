3f092427a2708da5fedddab93333d09d30e7bf0a
Sana Ullah Siddiqui <sanaullah.siddiqui0317@gmail.com>
2026-03-18 02:05:37 +0500

    learn react folder structure and components works

---
+++ folder_01/src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+        <h1>Its our my card</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nobis quas laborum et vel officiis iste? At ipsa soluta quod dicta consectetur, ipsam natus. Eaque amet est voluptatem. Illo, numquam!</p>
+      
+    </div>
+  )
+}
+
+export default card
