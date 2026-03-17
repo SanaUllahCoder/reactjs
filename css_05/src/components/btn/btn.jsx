ae6f852f40da1de066c4ee816ecee731f7742862
Sana Ullah Siddiqui <sanaullah.siddiqui0317@gmail.com>
2026-03-18 04:07:58 +0500

    learn styling with css

---
+++ css_05/src/components/btn/btn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './btn.module.css'
+
+const Btn  = () => {
+  return (
+    <div>
+        <button className={styles.btn}>Click Me</button>
+      
+    </div>
+  )
+}
+
+export default Btn
---
+++ css_05/src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+
+        <h1>Sana Ullah Siddiqui</h1>
+        <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex exercitationem aspernatur illo beatae quia, blanditiis eveniet sit ipsa facilis expedita fugiat temporibus atque nihil vero magnam, labore, culpa harum maiores.</p>
+      
+    </div>
+  )
+}
+
+export default Header
